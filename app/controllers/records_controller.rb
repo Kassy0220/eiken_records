f001f0808f7a643fa7cfcfea1e2237aa3fc23c02
Kassy0220 <shinbokutaro0220@gmail.com>
2023-12-15 18:33:56 +0900

    Add listing records and show record

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 class RecordsController < ApplicationController
+  def index
+    @records = Record.all
+  end
+
+  def show
+    @record = Record.find(params[:id])
+  end
+
   def new
     @record = Record.new
   end
